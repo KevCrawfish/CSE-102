6bc349e573ec8a3585cc84c6133657ecd286fed2
Kevin Mancio Crawford <kemcrawf@unix6.lt.ucsc.edu>
2021-11-20 18:37:20 -0800

    did some stuff

--- Graph.h
+++ Graph.h
@@ -3,7 +3,11 @@
 // Graph.h
 // Header file for Graph ADT
 //-----------------------------------------------------------------------------
-#include<iostream>
+#include <vector>
+#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
 
 #ifndef Graph_H_INCLUDE_
 #define Graph_H_INCLUDE_
@@ -12,9 +16,25 @@
 class Graph{
 
 private:
+  std::vector<std::tuple <int, int, int, int>> G;
+  std::vector<std::tuple <int, int, int, int>> M;
+
+  int *parent;
+  int *rank;
+  int vertices;
 
 public:
-
+  Graph(int n){
+    vertices = n;
+    parent = new int[n];
+    rank = new int[n];
+  }
+  int findSet(int x);
+  void makeSet(int x);
+  void addEdge(int u, int v, int w, int e);
+  void unions(int x, int y);
+  void link(int x, int y);
+  void mstKruskal(int edges, int vertices);
 };
 
 
